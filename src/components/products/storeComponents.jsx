40ddd1a7b67475fb06adea739a4a5d9086a127f7
MateoNerli <mnerli2003@gmail.com>
2023-11-21 21:21:29 -0300

    Se agrego la paginacion de usuarios y productos

--- src/components/products/storeComponents.jsx
+++ src/components/products/storeComponents.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import { Products } from "./products";
 import { useFetch } from "../../hooks/useFetch";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export const StoreComponent = () => {
   const { data, loading, error } = useFetch(
     "http://localhost:3000/api/products"
   );
-  // console.log(data);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(10);
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = data
+    ? data.data.slice(indexOfFirstProduct, indexOfLastProduct)
+    : [];
 
   return (
     <>
@@ -25,18 +36,42 @@ export const StoreComponent = () => {
         )}
         <div className="p-4 mt-14">
           <div className="grid grid-cols-1 gap-4 mb-8 rounded-lg shadow-sm md:mb-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-            {data
-              ? data.data.map((product) => (
-                  <Products
-                    key={product.id}
-                    name={product.name}
-                    price={product.price}
-                    description={product.description}
-                    category={product.category}
-                    img={product.img}
-                  />
-                ))
-              : null}
+            {currentProducts.map((product) => (
+              <Products
+                key={product.id}
+                name={product.name}
+                price={product.price}
+                description={product.description}
+                category={product.category}
+                img={product.img}
+              />
+            ))}
+          </div>
+          <div className="flex justify-center ">
+            {currentPage > 1 && (
+              <button
+                className="flex mr-10 justify-center items-center gap-2 w-10 h-12 cursor-pointer rounded-md shadow-2xl text-white font-semibold bg-gradient-to-r from-[#fb7185] via-[#e11d48] to-[#be123c] hover:shadow-xl hover:shadow-red-500 hover:scale-105 duration-300 hover:from-[#be123c] hover:to-[#fb7185]"
+                onClick={() => setCurrentPage((page) => Math.max(page - 1, 1))}
+              >
+                <FontAwesomeIcon icon={faArrowLeft} />
+              </button>
+            )}
+            {currentPage <
+              Math.ceil((data ? data.data.length : 0) / productsPerPage) && (
+              <button
+                className="flex justify-center items-center gap-2 w-10 h-12 cursor-pointer rounded-md shadow-2xl text-white font-semibold bg-gradient-to-r from-[#fb7185] via-[#e11d48] to-[#be123c] hover:shadow-xl hover:shadow-red-500 hover:scale-105 duration-300 hover:from-[#be123c] hover:to-[#fb7185]"
+                onClick={() =>
+                  setCurrentPage((page) =>
+                    Math.min(
+                      page + 1,
+                      Math.ceil((data ? data.data.length : 0) / productsPerPage)
+                    )
+                  )
+                }
+              >
+                <FontAwesomeIcon icon={faArrowRight} />
+              </button>
+            )}
           </div>
         </div>
       </div>
--- src/components/user/userComponent.jsx
+++ src/components/user/userComponent.jsx
@@ -1,9 +1,19 @@
 import { useFetch } from "../../hooks/useFetch";
 import { User } from "./user";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 export const UserComponent = () => {
   const { data, loading, error } = useFetch("http://localhost:3000/api/users");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(10);
 
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentUsers = data
+    ? data.data.slice(indexOfFirstUser, indexOfLastUser)
+    : [];
   return (
     <div className="p-4 sm:ml-64">
       {error && (
@@ -27,18 +37,42 @@ export const UserComponent = () => {
       )}
       <div className="p-4 mt-14">
         <div className="grid grid-cols-1 gap-4 mb-8 rounded-lg shadow-sm md:mb-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-          {data
-            ? data.data.map((user) => (
-                <User
-                  key={user.id}
-                  name={user.name}
-                  username={user.username}
-                  email={user.email}
-                  country={user.country}
-                  img={user.img}
-                />
-              ))
-            : null}
+          {currentUsers.map((user) => (
+            <User
+              key={user.id}
+              name={user.name}
+              username={user.username}
+              email={user.email}
+              country={user.country}
+              img={user.img}
+            />
+          ))}
+        </div>
+        <div className="flex justify-center ">
+          {currentPage > 1 && (
+            <button
+              className="flex mr-10 justify-center items-center gap-2 w-10 h-12 cursor-pointer rounded-md shadow-2xl text-white font-semibold bg-gradient-to-r from-[#fb7185] via-[#e11d48] to-[#be123c] hover:shadow-xl hover:shadow-red-500 hover:scale-105 duration-300 hover:from-[#be123c] hover:to-[#fb7185]"
+              onClick={() => setCurrentPage((page) => Math.max(page - 1, 1))}
+            >
+              <FontAwesomeIcon icon={faArrowLeft} />
+            </button>
+          )}
+          {currentPage <
+            Math.ceil((data ? data.data.length : 0) / usersPerPage) && (
+            <button
+              className="flex justify-center items-center gap-2 w-10 h-12 cursor-pointer rounded-md shadow-2xl text-white font-semibold bg-gradient-to-r from-[#fb7185] via-[#e11d48] to-[#be123c] hover:shadow-xl hover:shadow-red-500 hover:scale-105 duration-300 hover:from-[#be123c] hover:to-[#fb7185]"
+              onClick={() =>
+                setCurrentPage((page) =>
+                  Math.min(
+                    page + 1,
+                    Math.ceil((data ? data.data.length : 0) / usersPerPage)
+                  )
+                )
+              }
+            >
+              <FontAwesomeIcon icon={faArrowRight} />
+            </button>
+          )}
         </div>
       </div>
     </div>
